d63f6101aa9f48ae09c2b7def89c74f24ff022dc
student <student@ip-172-31-54-195.us-west-2.compute.internal>
2020-12-19 17:51:39 -0800

    DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/_05_minion/Minion.java
+++ src/_05_minion/Minion.java
@@ -1,34 +1,57 @@
 package _05_minion;
 
 public class Minion {
-
+	private String name; 
+		private int eyes; 
+		private String color; 
+		private String master;
 	public Minion(String string, int i, String string2, String string3) {
 		// TODO Auto-generated constructor stub
+this.name=string;
+this.eyes=i;
+this.color=string2;
+this.master=string3;
 	}
 
-	public Object getName() {
+
+	public String getName() {
 		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
-	public Object getEyes() {
+	public int getEyes() {
 		// TODO Auto-generated method stub
-		return null;
+		return eyes;
 	}
 
-	public Object getColor() {
+	public String getColor() {
 		// TODO Auto-generated method stub
-		return null;
+		return color;
 	}
 
-	public void setMaster(String string) {
+	public void setMaster(String smaster) {
 		// TODO Auto-generated method stub
-		
+		this.master=smaster;
+	}
+	
+	public void setColor(String scolor) {
+		// TODO Auto-generated method stub
+		this.color=scolor;
+	}
+	
+	public void setEyes(int seyes) {
+		// TODO Auto-generated method stub
+		this.eyes=seyes;
+	}
+	
+	public void setName(String sname) {
+		// TODO Auto-generated method stub
+		this.name=sname;
 	}
 
-	public Object getMaster() {
+	public String getMaster() {
 		// TODO Auto-generated method stub
-		return null;
+		return master;
 	}
 
 }
